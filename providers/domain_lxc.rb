10a0252b738e678b1ab9171ef36b4fe2deb3072e
Michał Łomnicki <michal.lomnicki@gmail.com>
2012-12-16 15:50:07 +0100

    Add support for LXC domains.
    Now it's possible to create and run LXC containers.
    
    Due to restrictions in the libvirt-lxc driver the
    domain must be defined along with filesystems and network.
    Neither a filesystem nor a network can be attached runtime.
    It makes the LXC provider less elastic than KVM but it covers
    the most frequent usecase.
    
    While this provider requires a filesystem and a network, the
    appropriate attributes has been added to the resource definition.
    The are ignored by KVM though. On the other hand the lxc provider
    ignores the arch attribute required by KVM.
    Maybe the domain resource should be split into lxc_domain and kvm_domain.

---
+++ providers/domain_lxc.rb
@@ -0,0 +1,46 @@
+require 'uuidtools'
+
+def load_current_resource
+  @current_resource = Chef::Resource::LibvirtDomain.new(new_resource.name)
+  @hypervisor = Vpslab::Libvirt::Hypervisor.new(new_resource.uri)
+  @current_resource
+end
+
+action :define do
+  unless @hypervisor.domain_defined?(domain_name)
+    domain_xml = Tempfile.new(new_resource.name)
+    t = template domain_xml.path do
+      cookbook "libvirt"
+      source   "lxc_domain.xml"
+      variables(
+        :name   => domain_name,
+        :vcpu   => new_resource.vcpu,
+        :memory => Vpslab::Libvirt::Helpers.to_bytes(new_resource.memory),
+        :uuid   => ::UUIDTools::UUID.random_create,
+        :filesystems => new_resource.filesystems,
+        :network => new_resource.network
+      )
+      action :nothing
+    end
+    t.run_action(:create)
+
+    @hypervisor.define_domain(::File.read(domain_xml.path))
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+action :autostart do
+  if @hypervisor.autostart_domain(domain_name)
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+action :create do
+  if @hypervisor.create_domain(domain_name)
+    new_resource.updated_by_last_action(true)
+  end
+end
+
+def domain_name
+  new_resource.name
+end
--- resources/domain.rb
+++ resources/domain.rb
@@ -8,4 +8,6 @@ end
 attribute :vcpu, :kind_of => [Integer, String], :required => true
 attribute :memory, :kind_of => [Integer, String], :required => true
 attribute :arch, :kind_of => String, :required => true
+attribute :filesystems, :kind_of => Array
+attribute :network, :kind_of => String
 attribute :uri, :kind_of => String, :default => 'qemu:///system'
